7403971ce9a994d5a77f5fdbbc54f4d70687e58d
Gustavo Maia <gu.mmeneses@gmail.com>
2025-07-26 15:27:22 -0300

    fix quickscope highlighting on Nvim-Tree

--- lua/plugins/quickscope.lua
+++ lua/plugins/quickscope.lua
@@ -2,6 +2,10 @@ return {
   {
     "unblevable/quick-scope",
     event = "VeryLazy",
+    config = function()
+      vim.g.qs_buftype_blacklist = { 'nofile', 'nowrite', 'quickfix', 'prompt', 'NvimTree' }
+      vim.g.qs_highlight_on_keys = { 'f', 'F', 't', 'T' }
+    end,
     init = function()
       vim.cmd [[
         augroup qs_colors
